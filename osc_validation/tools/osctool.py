28af8193e45def6a2371ab011cf8d9f8a2bdacf4
Thomas Sedlmayer <tsedlmayer@pmsfit.de>
2025-06-18 15:04:53 +0200

    Add docstring for OSCTool base class
    Signed-off-by: Thomas Sedlmayer <tsedlmayer@pmsfit.de>

--- osc_validation/tools/osctool.py
+++ osc_validation/tools/osctool.py
@@ -6,4 +6,13 @@ class OSCTool:
         self.tool_path = Path(tool_path) if tool_path else None
 
     def run(self, osc_path, odr_path, osi_path):
+        """
+        Executes the OpenSCENARIO XML engine with the specified input files and parameters.
+        Args:
+            osc_path (str): Path to the OpenSCENARIO (.xosc) file.
+            odr_path (str): Path to the OpenDRIVE (.xodr) file.
+            osi_path (str): Path for the output OSI SensorView file.
+        Returns:
+            str: Path for the output OSI SensorView file.
+        """
         raise NotImplementedError("This method must be implemented by the subclass.")
