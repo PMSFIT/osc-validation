4b58d8a85ae093894aed8c9036718900ccb13dec
Thomas Sedlmayer <tsedlmayer@pmsfit.de>
2026-04-28 17:25:04 +0200

    Add close-to-trajectory init poses
    Signed-off-by: Thomas Sedlmayer <tsedlmayer@pmsfit.de>

--- osc_validation/generation/init_transforms/__init__.py
+++ osc_validation/generation/init_transforms/__init__.py
@@ -1,11 +1,12 @@
 from .init_pose import (
     apply_init_pose_from_trajectory_start_transform,
-    apply_init_pose_from_trajectory_start_to_xosc,
     apply_init_pose_overrides_to_xosc,
     apply_init_pose_transform,
+    build_init_pose_overrides_from_close_to_trajectory_start,
     build_init_pose_overrides_from_xosc_init,
     build_init_pose_overrides_from_trajectory_start,
     build_init_pose_overridden_reference_trace,
+    compute_close_to_trajectory_start_xy,
 )
 from .models import (
     InitPoseOverride,
@@ -20,8 +21,9 @@ __all__ = [
     "InitPoseTransformResult",
     "InitPoseTransformSpec",
     "apply_init_pose_from_trajectory_start_transform",
-    "apply_init_pose_from_trajectory_start_to_xosc",
     "apply_init_pose_overrides_to_xosc",
+    "compute_close_to_trajectory_start_xy",
+    "build_init_pose_overrides_from_close_to_trajectory_start",
     "build_init_pose_overrides_from_xosc_init",
     "build_init_pose_overrides_from_trajectory_start",
     "build_init_pose_overridden_reference_trace",
