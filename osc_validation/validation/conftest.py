ae3a83d438fba5e8d98b082d304b0566a26b93e6
Thomas Sedlmayer <tsedlmayer@pmsfit.de>
2026-06-15 14:40:22 +0200

    Add pytest tmp paths to report extras
    Signed-off-by: Thomas Sedlmayer <tsedlmayer@pmsfit.de>

--- osc_validation/validation/conftest.py
+++ osc_validation/validation/conftest.py
@@ -37,20 +37,31 @@ def pytest_runtest_makereport(item: pytest.Item, call):
     report.validation_feature = _marker_value(item, "validation_feature")
 
     description = _test_description(item)
-    if not description:
-        return
 
     pytest_html = item.config.pluginmanager.getplugin("html")
     if pytest_html is None:
         return
 
-    escaped_description = html.escape(description).replace("\n", "<br>")
     extras = getattr(report, "extras", [])
-    extras.append(
-        pytest_html.extras.html(
-            f"<div><strong>Validation description</strong><br>{escaped_description}</div>"
+    tmp_path = item.funcargs.get("tmp_path")
+    if tmp_path is not None:
+        resolved_tmp_path = tmp_path.resolve()
+        escaped_tmp_path = html.escape(str(resolved_tmp_path))
+        escaped_tmp_path_uri = html.escape(resolved_tmp_path.as_uri(), quote=True)
+        extras.append(
+            pytest_html.extras.html(
+                "<div><strong>Pytest temp directory</strong><br>"
+                f'<a href="{escaped_tmp_path_uri}">{escaped_tmp_path}</a></div>'
+            )
         )
-    )
+
+    if description:
+        escaped_description = html.escape(description).replace("\n", "<br>")
+        extras.append(
+            pytest_html.extras.html(
+                f"<div><strong>Validation description</strong><br>{escaped_description}</div>"
+            )
+        )
     report.extras = extras
 
 
